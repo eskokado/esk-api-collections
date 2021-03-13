a504a04e30eae481a8b7826e70d68c688504dd11
Edson Shideki Kokado <eskokado@gmail.com>
2021-03-13 10:48:09 -0300

    Other operations double array test class

--- src/main/kotlin/one/digitalinnovation/collections/TesteOperacoes.kt
+++ src/main/kotlin/one/digitalinnovation/collections/TesteOperacoes.kt
@@ -13,4 +13,16 @@ fun main() {
     println("Salários maior que 2500")
     val salariosMaiorQue2500 = salarios.filter { it > 2500.0 }
     salariosMaiorQue2500.forEach { println(it) }
+
+    println("----------------------------")
+    println("Count: ${salarios.count { it in 2000.0..3500.0}}")
+
+    println("----------------------------")
+    println("Existe: ${salarios.find { it == 2500.0}}")
+    println("Não Existe: ${salarios.find { it == 700.0}}")
+
+    println("----------------------------")
+    println("Existe Salários igual a 2500.0: ${salarios.any { it == 2500.0}}")
+    println("Existe Salários igual a 700.0: ${salarios.any { it == 700.0}}")
+
 }
